8662b6d77ce589a535eb5d3f38ba407ed2ddf171
tarantej <tarantejsingh@gmail.com>
2026-03-15 11:45:57 +0530

    Initial commit - Java tutorial code from Udemy Course by Navin Reddy

---
+++ hello.java
@@ -0,0 +1,56 @@
+class Hello
+{
+    public static void main(String a[])
+    {
+        // System.out.println("Hello World");
+
+        // Adding two numbers
+        int num1 = 3;
+        int num2 = 5;
+        int result = num1+num2;
+        System.out.println(result);
+    }
+}
+
+// Run java Hello (the class name on console)
+
+// JVM - Java Virtual MAchine
+// JRE - Java Runtime Environment - contains the JVM with additional libraries
+// JDK - Java Development Kit - Installed on the machine to enable Java code development
+// WORA - Write once run anywhere
+// Variable - Where you store your values of your data
+// Variable types - String, Int
+// Always first compile using javac file.java and then run with java filename
+// while we can use the print function like System.out.print, the line System.out.println prints the output in the next line
+
+// Data Types 
+
+// Lot of in built data
+
+// Primitive Data Types - Simple, Basic to use
+
+// Data Type Categories - 
+/*
+    Integer - byte, short, int, long 
+    Float - double, flat
+    Character -  char
+    Boolean - True, False
+
+    each type has its own size
+
+Integer Types
+    int - 4 bytes
+    long - 8 bytes
+    short - 2 bytes
+    Byte - 1 byte (8 bits) 
+    
+    Integer Range : -128 to 127
+    
+    Float - 4 bytes
+    Double - 8 bytes 
+    
+    Char - 2 bytes
+    
+    
+    
+*/
