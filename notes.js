abe68e049fb8d60783e996ab3b893f24861b4b46
Taran Tej <21302579+tarantej@users.noreply.github.com>
2026-03-31 18:27:50 +0530

    Java Conditionals: Evaluate Student Result with If-Else and Grading Logic

--- notes.js
+++ notes.js
@@ -69,3 +69,103 @@ a = b will work since integer has a much larger range of value than b. This is a
 
 //		int x = (int)f;
 
+// Java if else statement Test
+
+/*
+    Java Conditionals: Evaluate Student Result with If-Else and Grading Logic
+
+Student Performance Evaluation Using If-Else in Java
+
+Write a Java program to evaluate a student's performance using if-else and else-if statements.
+
+You are given the following:
+
+    An integer variable score = 75
+
+    A character variable grade = 'B'
+
+Your task is to:
+
+    Use if-else to check if the student passed or failed.
+
+        A score of 50 or above is considered a pass.
+
+        Print "Passed" or "Failed" accordingly.
+
+    Use else-if conditions to assign and print a grade based on the score:
+
+        A for 90 and above
+
+        B for 75 to 89
+
+        C for 60 to 74
+
+        D for below 60
+
+    Use System.out.println() to clearly display the output.
+
+Instructions:
+
+    The variables score and grade are already declared for you.
+
+    Use if-else to evaluate pass/fail status.
+
+    Use else-if to assign and print the grade based on the score.
+
+    Ensure the output formatting matches the example output.
+
+    Do not use ternary or switch statements in this exercise.
+
+
+Expected Output:
+
+  Passed
+
+  Grade:  B
+*/
+
+// Output
+
+/*
+
+    public class Exercise {
+   public static void main(String[] args) {
+       
+        int score = 75;
+        char grade = 'B';
+        
+        // Passed or Failed
+        
+       if(score>=50)
+       {
+           System.out.println("Passed");
+       }
+       else
+       {
+           System.out.println("Failed");
+       }
+       
+        // Grading
+        
+        if(score>=90)
+        {
+            System.out.println("Grade: A");
+        }
+        else if(score>=75 && score<=89)
+        {
+            System.out.println("Grade: B");
+        }
+        else if(score>=60 && score<=74)
+        {
+            System.out.println("Grade: C");
+        }
+        else
+        {
+            System.out.println("Grade: D");
+        }
+       
+   }
+}
+
+ */
+
