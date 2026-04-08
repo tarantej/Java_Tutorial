702caccb7a56470b9903eae9485d862d6cdaa9bd
tarantej <tarantejsingh@gmail.com>
2026-04-08 14:04:25 +0530

    Creating Dashboard App Prject using Class and Object

---
+++ Dashboard/Users.java
@@ -0,0 +1,27 @@
+class Users {
+
+//  Declare Variables
+String first_name;
+String last_name;
+String username;
+String password;
+public String welcome()
+{
+//return first_name and last_name in the method
+return "Welcome to the Dashboard, "+first_name+" "+last_name;
+}
+
+public static void main(String args[])
+{
+//  Create a new object u for class Users
+Users u = new Users();
+//  Call the first_name and last_name variables passed in the method and put it in a string variable to show output
+u.first_name = "Tarantej";
+u.last_name = "Singh";
+String message = u.welcome();
+System.out.println(message);
+
+}
+
+
+}
