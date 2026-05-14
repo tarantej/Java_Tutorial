0c5750fd0659473e2935e9eb5be4502b251b2d40
Taran Tej <21302579+tarantej@users.noreply.github.com>
2026-05-14 15:20:46 +0530

    Downloaded a Bootstrap Admin Template and integrated Thymeleaf in login and dashboard pages. Login Page working successfully

--- Dashboard/src/main/java/com/dashboard/DashboardController.java
+++ Dashboard/src/main/java/com/dashboard/DashboardController.java
@@ -3,33 +3,102 @@ package com.dashboard;
 import java.sql.*;
 
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 
-@RestController
+@Controller
 public class DashboardController {
-    @GetMapping("/")
-    public String home() throws Exception {
 
-        //  Database Connection
-        try
-        {
+    @GetMapping("/")
+    public String home()
+    {
+        return "index";
+    }
+
+//    Login
+
+    @GetMapping("/login")
+    public String loginPage()
+    {
+
+        return"login";
+    }
+
+    @PostMapping("/login")
+    public String login(@RequestParam("username") String username, @RequestParam("password") String password, Model model)
+    {
+        // Login Validation
+
+        try {
+
             Connection con = DriverManager.getConnection(
                     "jdbc:postgresql://localhost:5432/dashboard",
                     "postgres",
                     "12345"
             );
 
+            String query = "SELECT * FROM users WHERE username=? AND password=?";
 
-            return "Database Connected Successfully";
-//        DashboardUsers u = new DashboardUsers("Tarantej", "Singh");
-//
-//        return u.welcome();
+            PreparedStatement ps = con.prepareStatement(query);
+
+            ps.setString(1, username);
+            ps.setString(2, password);
+
+            ResultSet rs = ps.executeQuery();
+
+            if(rs.next()) {
+                String userName = rs.getString("username");
+                String firstName = rs.getString("first_name");
+                String lastName = rs.getString("last_name");
+
+                model.addAttribute(
+                        "username",userName
+                );
+
+                return "Dashboard/dashboard";
+            }
+
+            else {
+
+                model.addAttribute(
+                        "error",
+                        "Invalid Username or Password"
+                );
+
+                return "Login/login";
+            }
+
         }
 
-        catch (Exception e)
-        {
-            return e.getMessage();
-        }
+        catch(Exception e) {
 
+            e.printStackTrace();
+
+            model.addAttribute(
+                    "error",
+                    "Database Error"
+            );
+
+            return "Login/login";
+        }
+    }
+
+    // Logout
+
+    @GetMapping("/logout")
+    public String logout() {
+
+        return "redirect:/login";
+    }
+
+    //  Dashboard Page
+
+    @GetMapping("/dashboard")
+    public String Dashboard()
+    {
+        System.out.println("Welcome to the Dashboard");
+        return "Dashboard/dashboard";
     }
 }
